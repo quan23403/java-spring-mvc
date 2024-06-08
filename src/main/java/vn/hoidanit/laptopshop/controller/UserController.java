455b926c99d4322325ed0adfdbfc665573adaf84
quan234 <quan23403@gmail.com>
2024-06-08 21:42:46 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,14 +1,18 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 // @RestController
@@ -29,29 +33,45 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
+    // private final UserRepository userRepository;
     public UserController(UserService userService) {
         this.userService = userService;
+        // this.userRepository = userRepository;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        // System.out.println("sadasd");
+        // String test = this.userService.handleHello();
+        List<User> arrUsers = this.userService.getAllUsersByEmail("quan23403@gmail.com");
+        System.out.println(arrUsers);
+        model.addAttribute("eric", "test");
         model.addAttribute("hoidanit", "from TCLC");
         return "hello";
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        // model.addAttribute("newUser", new User());
+        List<User> users = this.userService.getAllUsers();
+        // System.out.println(users);
+        model.addAttribute("users1", users);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage() {
-        return "hello";
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        // System.out.println("run here " + hoidanit);
+        this.userService.handleSaveUser(hoidanit);
+        return "redirect:/admin/user";
     }
 
 }
--- src/main/webapp/WEB-INF/view/admin/user/create.jsp
+++ src/main/webapp/WEB-INF/view/admin/user/create.jsp
@@ -2,7 +2,6 @@
     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
         <%@taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
             <html lang="en">
-
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
